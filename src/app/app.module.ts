feedd2834ec98d895df1079e6d71a8cdebdd8cae
Eivy-646 <rodrigoevn@gmail.com>
2022-09-16 14:55:26 -0500

    comentarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
+import { ListarComentariosComponent } from './components/listar-comentarios/listar-comentarios.component';
+import { VerComentariosComponent } from './components/ver-comentarios/ver-comentarios.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AgregarEditarComentarioComponent,
+    ListarComentariosComponent,
+    VerComentariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
